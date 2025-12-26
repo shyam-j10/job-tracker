ca920dfada0c5b3c1dc2ae87fa638f482fa539af
shyam-j10 <jayashyam2020@gmail.com>
2025-12-26 17:46:15 +0530

    added email feature using smtp

--- notification-service/src/main/java/com/jobtracker/notification/kafka/JobEventConsumer.java
+++ notification-service/src/main/java/com/jobtracker/notification/kafka/JobEventConsumer.java
@@ -1,24 +1,53 @@
 package com.jobtracker.notification.kafka;
 
 import com.jobtracker.notification.kafka.event.JobEvent;
+import com.jobtracker.notification.service.EmailService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JobEventConsumer {
 
+    private final EmailService emailService;
+    private static final Logger log = LoggerFactory.getLogger(JobEventConsumer.class);
+
+
+    public JobEventConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @KafkaListener(
         topics = "job-events",
         groupId = "notification-group"
     )
     public void consume(JobEvent event) {
-
-        System.out.println("🔔 Notification received");
-        System.out.println("User ID: " + event.getUserId());
-        System.out.println("Job ID: " + event.getJobId());
-        System.out.println("Event Type: " + event.getEventType());
-        System.out.println("Description: " + event.getDescription());
-        System.out.println("Time: " + event.getTimestamp());
-        System.out.println("--------------------------------");
+        try{
+            String subject = "Job Update: " + event.getEventType();
+            
+            String body = """
+                Job ID: %s
+                Event: %s
+                Description: %s
+                Time: %s
+                """.formatted(
+                    event.getJobId(),
+                    event.getEventType(),
+                    event.getDescription(),
+                    event.getTimestamp()
+            );
+                    
+            emailService.sendJobNotification(
+                "jayashyam2020@gmail.com",
+                subject,
+                body
+            );
+                        
+            System.out.println("📧 Email sent for event: " + event.getEventType());
+        }catch(Exception e) {
+            log.error("Couldn't send email,skipping retry", e);
+        }
     }
 }
